19dedf204fb41fa9909a17b9237a312b673b3665
Simon <spam@esemi.ru>
2016-11-12 00:09:45 +0300

    release new select enemy for attack method - decrease priority of buildings and check can attack type

--- MyStrategy.py
+++ MyStrategy.py
@@ -6,24 +6,33 @@ from model.Move import Move
 from model.Wizard import Wizard
 from model.World import World
 from model.Faction import Faction
-from model.LivingUnit import LivingUnit
+from model.Building import Building
 
 
 class MyStrategy:
+    # shortcuts
+    W = None
+    G = None
+    STAFF_SECTOR = None
+    FRIENDLY_FACTION = None
+
     LOGS_ENABLE = True
     INITIATED = False
     WAY_POINTS = list()
     NEXT_WAYPOINT = 1
     PREV_WAYPOINT = 0
-    LOW_HP_FACTOR = 0.4
-    STAFF_SECTOR = None
-    FRIENDLY_FACTION = None
+
+    # количество тиков, которое мы пропускаем в начале боя
     PASS_TICK_COUNT = 10
+
+    # фактор для расстояния выстрела, используется для отступления на минимально необходимое расстояние
     WARNING_DISTANCE_FACTOR = 1.2
-    # максимальное количество врагов в ближней зоне
-    # если больше - нужно сваливать
+
+    # если здоровья меньше данного количества - задумываемся об отступлении
+    LOW_HP_FACTOR = 0.4
+
+    # максимальное количество врагов в ближней зоне, если больше - нужно сваливать
     MAX_ENEMIES_IN_STAFF_ZONE = 1
-    W = None
 
     def _init(self, game: Game, me: Wizard, world: World):
         self.W = world
@@ -55,42 +64,48 @@ class MyStrategy:
             self.log('initial cooldown pass turn')
             return
 
+        # todo
+        #       если у нас мало хп или много врагов в радиусе ближней атаки - двигаемся к предыдущему вейпоинту {strafe_speed, turn, speed}
+        #       иначе - двигаемся к базе врагов {strafe_speed, turn, speed}
+        # есть враги в зоне действия каста - атакуем, крутимся к цели если не находимся в отступлении {turn, action, cast_angle, min_cast_distance}
+
         # если ХП мало - стоим или отступаем
         if self._need_retreat(me):
-            self.log('retreat case')
+            self.log('retreat')
             if self._enemies_in_warning_distance_count(me):
                 self.log('retreat to home by low HP and enemies in attack range')
                 self._goto(self._get_prev_waypoint(me), move, me)
             return
 
-        # есть враги в радиусе обстрела
-        range_enemies, staff_enemies = self._get_enemies_for_attack(me)
-        if range_enemies or staff_enemies:
-            self.log('found range %d enemies and %d for staff attack' % (len(range_enemies), len(staff_enemies)))
-
-            attack_enemy, can_attack = self._select_enemy_for_attack(me, staff_enemies, range_enemies)
-            if not attack_enemy:
-                self.log('cooldown for attack - hold on place')
-                return
-
-            angle_to_enemy = me.get_angle_to_unit(attack_enemy)
-            if can_attack:
-                self.log('select enemy for attack %s' % attack_enemy.id)
-                move.cast_angle = angle_to_enemy
-                if self._enemy_in_range_attack_distance(me, game, attack_enemy):
-                    self.log('cast attack')
-                    move.action = ActionType.MAGIC_MISSILE
-                    move.min_cast_distance = me.get_distance_to_unit(
-                        attack_enemy) - attack_enemy.radius + self.G.magic_missile_radius
-                else:
-                    self.log('staff attack')
-                    move.action = ActionType.STAFF
-            else:
-                self.log('select enemy for turn %s' % attack_enemy.id)
-                move.turn = angle_to_enemy
-        else:
+        # если врагов в радиусе обстрела нет - идём к их базе
+        enemy_targets = self._enemies_in_attack_distance(me)
+        if not enemy_targets:
             self.log('move to next waypoint')
             self._goto(self._get_next_waypoint(me), move, me)
+            return
+
+        # есть враги в радиусе обстрела
+        self.log('found %d enemies for attack' % len(enemy_targets))
+        selected_enemy = self._select_enemy_for_attack(me, enemy_targets)
+        angle_to_enemy = me.get_angle_to_unit(selected_enemy)
+
+        # если цель не в секторе атаки - поворачиваемся к ней
+        if not self._enemy_in_attack_sector(me, selected_enemy):
+            self.log('select enemy for turn %s' % selected_enemy.id)
+            move.turn = angle_to_enemy
+        else:
+            # если можем атаковать - атакуем
+            self.log('select enemy for attack %s' % selected_enemy.id)
+            move.cast_angle = angle_to_enemy
+            if self._enemy_in_cast_distance(me, selected_enemy):
+                self.log('cast attack')
+                move.action = ActionType.MAGIC_MISSILE
+                move.min_cast_distance = (me.get_distance_to_unit(selected_enemy) -
+                                          selected_enemy.radius +
+                                          self.G.magic_missile_radius)
+            else:
+                self.log('staff attack')
+                move.action = ActionType.STAFF
 
     def _get_prev_waypoint(self, me: Wizard):
         wp = self.WAY_POINTS[self.PREV_WAYPOINT]
@@ -118,7 +133,8 @@ class MyStrategy:
 
     def _find_problem_units(self, me: Wizard):
         units = self.W.buildings + self.W.wizards + self.W.minions + self.W.trees
-        connected_u = [t for t in units if me.get_distance_to_unit(t) <= (me.radius + t.radius) * 1.05 and me.id != t.id]
+        connected_u = [t for t in units if
+                       me.get_distance_to_unit(t) <= (me.radius + t.radius) * 1.05 and me.id != t.id]
         problem_u = [t for t in connected_u if fabs(me.get_angle_to_unit(t)) < self.PROBLEM_ANGLE]
         return None if not problem_u else problem_u[0]
 
@@ -139,33 +155,9 @@ class MyStrategy:
             if fabs(angle) < self.STAFF_SECTOR / 4.0:
                 move.speed = self.MAX_SPEED
 
-    def _get_enemies_for_attack(self, me: Wizard):
-        enemy_targets = self._get_enemies()
-        cast_attack = [t for t in enemy_targets if self._enemy_in_range_attack_distance(me, self.G, t)]
-        staff_attack = [t for t in enemy_targets if self._can_staff_attack_enemy(me, self.G, t)]
-        return cast_attack, staff_attack
-
-    @staticmethod
-    def _enemy_in_range_attack_distance(me: Wizard, game: Game, e: LivingUnit):
-        distance = me.get_distance_to_unit(e)
-        return (game.staff_range + e.radius) < distance <= me.cast_range
-
-    @staticmethod
-    def _can_staff_attack_enemy(me: Wizard, game: Game, e):
-        distance = me.get_distance_to_unit(e)
-        return distance <= (game.staff_range + e.radius)
-
-    @staticmethod
-    def _can_range_attack(me: Wizard, game: Game):
-        return not me.remaining_cooldown_ticks_by_action[ActionType.MAGIC_MISSILE] and \
-               game.magic_missile_manacost <= me.mana
-
-    def _check_enemy_in_attack_sector(self, me: Wizard, e):
-        return fabs(me.get_angle_to_unit(e)) < self.STAFF_SECTOR / 2.0
-
-    def _select_enemy_for_attack(self, me: Wizard, staff_enemies: list, range_enemies: list):
+    def _select_enemy_for_attack(self, me: Wizard, enemy_targets: list):
         def _filter_into_attack_sector(el: list):
-            return [e for e in el if self._check_enemy_in_attack_sector(me, e)]
+            return [e for e in el if self._enemy_in_attack_sector(me, e)]
 
         def _sort_by_hp(el: list):
             return sorted(el, key=lambda u: u.life)
@@ -173,29 +165,48 @@ class MyStrategy:
         def _sort_by_angle(el: list):
             return sorted(el, key=lambda u: fabs(me.get_angle_to_unit(u)))
 
-        # если есть враги в радиусе ближней атаки
-        staff_in_attack_sector = _filter_into_attack_sector(staff_enemies)
-        if staff_in_attack_sector:
-            e = _sort_by_hp(staff_in_attack_sector)[0]
-            self.log('select enemy for staff attack %s' % e.id)
-            return e, True
+        def _sort_by_distance(el: list):
+            return sorted(el, key=lambda u: me.get_distance_to_unit(u))
 
-        # если у нас есть возможность кастовать - то ищем цель и среди удалённых
-        all_enemies = (staff_enemies + range_enemies) if self._can_range_attack(me, self.G) else staff_enemies
+        def _filter_can_attack_now(el: list):
+            result = []
+            if not me.remaining_action_cooldown_ticks:
+                for e in el:
+                    if self._enemy_in_cast_distance(me, e) and self.G.magic_missile_manacost <= me.mana and \
+                            not me.remaining_cooldown_ticks_by_action[ActionType.MAGIC_MISSILE]:
+                        result.append(e)
+                    elif self._enemy_in_staff_distance(me, e) and \
+                            not me.remaining_cooldown_ticks_by_action[ActionType.STAFF]:
+                        result.append(e)
+            return result
 
-        if not all_enemies:
-            return None, False
+        living_units = [e for e in enemy_targets if not isinstance(e, Building)]
+        buildings = [e for e in enemy_targets if isinstance(e, Building)]
 
-        # find can attacked targets
-        enemies_in_attack_sector = _filter_into_attack_sector(all_enemies)
-        if enemies_in_attack_sector:
-            e = _sort_by_hp(enemies_in_attack_sector)[0]
-            self.log('select enemy in attack sector %s' % e.id)
-            return e, True
+        # если есть живые враги
+        if living_units:
+            self.log('found living enemies %d' % len(living_units))
+            enemies_in_sector = _filter_into_attack_sector(living_units)
 
-        e = _sort_by_angle(all_enemies)[0]
-        self.log('select enemy for turn %s' % e.id)
-        return e, False
+            if enemies_in_sector:  # выбираем того, кого мы сможем ударить прямо сейчас
+                self.log('found enemies in attack sector %d' % len(enemies_in_sector))
+                enemies_can_attack_now = _filter_can_attack_now(enemies_in_sector)
+                if enemies_can_attack_now:
+                    e = _sort_by_hp(enemies_can_attack_now)[0]
+                    self.log('select enemy for attack now by HP %s' % e.id)
+                else:
+                    e = _sort_by_distance(enemies_in_sector)[0]
+                    self.log('select nearest enemy for delayed attack (turn now) %s' % e.id)
+
+            else:  # если таких нет - ищем ближайшего по углу
+                self.log('all living enemies not in attack sector')
+                e = _sort_by_angle(living_units)[0]
+                self.log('select enemy for turn %s' % e.id)
+
+        else:  # если никого кроме зданий во врагах нет - мочим самое слабое здание
+            e = _sort_by_hp(buildings)[0]
+            self.log('select building for attack %s' % e.id)
+        return e
 
     def _need_retreat(self, me: Wizard):
         enemies_in_staff_zone = self._enemies_in_staff_distance(me)
@@ -213,12 +224,27 @@ class MyStrategy:
         self.log('found %d enemies in warning zone' % len(danger_enemies))
         return len(danger_enemies) > 0
 
-    def _enemies_in_staff_distance(self, me: Wizard):
+    def _enemies_in_attack_distance(self, me: Wizard):
         enemies = self._get_enemies()
         danger_enemies = [e for e in enemies
-                          if me.get_distance_to_unit(e) <= (self.G.staff_range + e.radius)]
+                          if self._enemy_in_staff_distance(me, e) or self._enemy_in_cast_distance(me, e)]
+        self.log('found %d enemies in cast zone' % len(danger_enemies))
+        return danger_enemies
+
+    def _enemies_in_staff_distance(self, me: Wizard):
+        enemies = self._get_enemies()
+        danger_enemies = [e for e in enemies if self._enemy_in_staff_distance(me, e)]
         self.log('found %d enemies in staff zone' % len(danger_enemies))
         return danger_enemies
+
+    def _enemy_in_staff_distance(self, me: Wizard, e):
+        return me.get_distance_to_unit(e) <= (self.G.staff_range + e.radius)
+
+    def _enemy_in_cast_distance(self, me: Wizard, e):
+        return me.get_distance_to_unit(e) <= me.cast_range and not self._enemy_in_staff_distance(me, e)
+
+    def _enemy_in_attack_sector(self, me: Wizard, e):
+        return fabs(me.get_angle_to_unit(e)) <= self.STAFF_SECTOR / 2.0
 
     def log(self, message):
         if self.LOGS_ENABLE:
